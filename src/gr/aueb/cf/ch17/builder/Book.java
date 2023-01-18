b8c4bad9d7cb7d412eacb14d6d5ca34ff2998a81
GVogiatzis <gevogiatzhs@gmail.com>
2023-01-18 22:57:19 +0200

    ch17

---
+++ src/gr/aueb/cf/ch17/builder/Book.java
@@ -0,0 +1,70 @@
+package gr.aueb.cf.ch17.builder;
+
+public class Book {
+    private final long id;
+    private final String isbn;
+    private final String author;
+    private final String title;
+
+    public static class Builder {
+        //required fields
+        private final  long id;
+        private final String isbn;
+
+        //optional fields -- init to default values
+        private String author = "";
+        private String title = "";
+
+        public Builder(long id, String isbn) {
+            this.id = id;
+            this.isbn = isbn;
+        }
+
+        public Builder author(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return  this;
+        }
+
+        public Book build() {
+            return new Book(this);
+        }
+    }
+
+    private Book(Builder builder) {
+        this.id = builder.id;
+        this.author = builder.author;
+        this.isbn = builder.isbn;
+        this.title = builder.title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
---
+++ src/gr/aueb/cf/ch17/runna/IMission.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.ch17.runna;
+
+public interface IMission {
+    MissionStatus getStatus();
+    void setStatus(MissionStatus missionStatus);
+    void embark();
+
+}
