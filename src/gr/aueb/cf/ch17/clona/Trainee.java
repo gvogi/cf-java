79fb14e8b94327f74efb0eaef4a5fe0350128672
GVogiatzis <gevogiatzhs@gmail.com>
2023-01-30 21:32:04 +0200

    Minor changes

--- src/gr/aueb/cf/ch17/clona/Trainee.java
+++ src/gr/aueb/cf/ch17/clona/Trainee.java
@@ -1,13 +1,12 @@
 package gr.aueb.cf.ch17.clona;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 public class Trainee implements Cloneable, Serializable {
     private static final long serialVersionUID = 1L;
     private String name;
     private City city;
-    private transient int hashcode; // not serializable εξαιρείται απο το serialization
+    private transient int hashcode; // not serializable εξαιρείται απο το serialization λόγω του ότι έχει δηλωθεί transient.
 
     public Trainee() {
     }
@@ -54,47 +53,6 @@ public class Trainee implements Cloneable, Serializable {
         trainee.setCity(city);
         return trainee;
     }
-
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Trainee trainee = (Trainee) o;
-//        return Objects.equals(getName(), trainee.getName()) && Objects.equals(getCity(), trainee.getCity());
-//    }
-
-//    @Override
-//    public int hashCode() {
-//        final int prime = 31;
-//        int result = 1;
-//
-//        result = prime * result + ((name == null) ? 0 : name.hashCode());
-//        result = prime * result + ((city == null) ? 0 : city.hashCode());
-//
-//        return result;
-//    }
-
-
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//
-//        Trainee trainee = (Trainee) o;
-//
-//        if (hashcode != trainee.hashcode) return false;
-//        if (getName() != null ? !getName().equals(trainee.getName()) : trainee.getName() != null) return false;
-//        return getCity() != null ? getCity().equals(trainee.getCity()) : trainee.getCity() == null;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        int result = getName() != null ? getName().hashCode() : 0;
-//        result = 31 * result + (getCity() != null ? getCity().hashCode() : 0);
-//        result = 31 * result + hashcode;
-//        return result;
-//    }
-
 
     @Override
     public boolean equals(Object o) {
